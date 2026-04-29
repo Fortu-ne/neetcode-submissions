c7a0b2ab75f6208f9aaf7747bd1f3e2324d7de63
Fortune Nkambule <89359520+Fortu-ne@users.noreply.github.com>
2026-04-29 21:05:25 +0200

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() < 1 || t.length() < 1 || s.length() != t.length()){
+            return  false;
+        }
+        
+        char[] tChar = t.toCharArray();
+        char[] sChar = s.toCharArray();
+        
+        Arrays.sort(tChar);
+        Arrays.sort(sChar);
+        
+        
+        return Arrays.equals(tChar,sChar);
+    }
+}
