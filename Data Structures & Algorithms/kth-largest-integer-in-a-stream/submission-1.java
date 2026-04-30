954816300c99a698d3a74ce94960eafaf2faf4c3
Fortune Nkambule <89359520+Fortu-ne@users.noreply.github.com>
2026-04-30 22:33:42 +0200

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.java
@@ -0,0 +1,29 @@
+class KthLargest {
+private static int k;
+    public static PriorityQueue<Integer> minHeap;
+    private static int[] arrayHeap;
+    
+    public KthLargest(int k, int[] nums) {
+
+        minHeap = new PriorityQueue<>();;
+        this.k = k;
+        arrayHeap = nums;
+
+        for( int n : arrayHeap){
+            add(n);
+        }
+
+
+    }
+    
+    public int add(int val) {
+               
+        minHeap.offer(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll(); // remove smallest, keep only k largest
+        }
+
+        return minHeap.peek(); // kth largest
+    }
+}
