5a05b43e0e12ec7c8cbd8e1789466c6aafe01e95
Fortune Nkambule <89359520+Fortu-ne@users.noreply.github.com>
2026-07-17 12:09:08 +0200

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+         
+          if(nums.length < 1) return 0;
+
+
+      HashMap<Integer, Integer> hash = new HashMap<>();
+
+
+      for(int n : nums){
+        hash.put(n ,hash.getOrDefault(n,0) + 1);
+      }    
+
+     List<Map.Entry<Integer,Integer>> list = new ArrayList<>(hash.entrySet());
+
+       list.sort(Comparator.comparing(Map.Entry<Integer,Integer>::getValue).reversed());
+
+
+       return list.get(0).getKey();
+
+    }
+}
